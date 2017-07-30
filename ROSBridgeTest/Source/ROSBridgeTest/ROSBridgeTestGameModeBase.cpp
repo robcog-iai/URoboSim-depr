96f81c6f0e286be847c819a2bf28b09c10d58e6b
Li Yilong <gnoliyil@pku.edu.cn>
2017-07-30 13:21:27 +0800

    Add service support

--- ROSBridgeTest/Source/ROSBridgeTest/ROSBridgeTestGameModeBase.cpp
+++ ROSBridgeTest/Source/ROSBridgeTest/ROSBridgeTestGameModeBase.cpp
@@ -3,6 +3,72 @@
 #include "ROSBridgeTestGameModeBase.h"
 #include "ROSBridgeTest.h"
 
+void AROSBridgeTestGameModeBase::BeginPlay()
+{
+    Super::BeginPlay();
+    // AROSBridgeActor* Actor = GetWorld()->SpawnActor<AROSBridgeActor>(AROSBridgeActor::StaticClass());
+    Handler = new FROSBridgeHandler(TEXT("127.0.0.1"), 9001);
+    UE_LOG(LogTemp, Log, TEXT("Handler Created. "));
 
+    Subscriber = new FROSStringSubScriber(TEXT("/chatter"));
+    Handler->AddSubscriber(Subscriber);
+    UE_LOG(LogTemp, Log, TEXT("Added chatter subscriber. "));
 
+    /* Publisher = new FROSBridgePublisher(TEXT("sensor_msgs/JointState"), TEXT("/talker"));
+    Handler->AddPublisher(Publisher); */
 
+    ServiceClient = new FROSAddTwoIntsClient(TEXT("add_two_ints"));
+
+    Handler->Connect();
+    UE_LOG(LogTemp, Log, TEXT("Connected to WebSocket server. "));
+}
+
+void AROSBridgeTestGameModeBase::Tick(float DeltaSeconds)
+{
+
+    Super::Tick(DeltaSeconds);
+    UE_LOG(LogTemp, Log, TEXT("GameMode Ticks, DeltaSeconds = %.3f."), DeltaSeconds);
+
+    // FROSBridgeMsgStdmsgsString* StringMsgToSend = new
+    //     FROSBridgeMsgStdmsgsString(TEXT("New Message at ") + FDateTime::Now().ToString());
+    // Handler->PublishMsg(TEXT("/talker"), StringMsgToSend);
+    // delete StringMsgToSend;
+
+    /* FROSBridgeMsgStdmsgsHeader header(GFrameCounter, FROSTime::Now(), TEXT("1"));
+    TArray<FString> names = { TEXT("head"), TEXT("torso"), TEXT("gripper") };
+    TArray<double > position, velocity, effort;
+    for (int i = 0; i < 3; i++)
+    {
+        position.Add(FMath::FRandRange(0.0f, 5.0f));
+        velocity.Add(FMath::FRandRange(0.0f, 5.0f));
+        effort.Add(FMath::FRandRange(0.0f, 5.0f));
+    }
+    FROSBridgeMsgSensormsgsJointState *JointState
+        = new FROSBridgeMsgSensormsgsJointState(header, names, position, velocity, effort);
+    Handler->PublishMsg(TEXT("/talker"), JointState);
+    delete JointState;  */
+
+    if (GFrameCounter % 10 == 0)
+    {
+        int NumA = FMath::RandRange(1, 10000);
+        int NumB = FMath::RandRange(1, 10000);
+        TSharedPtr<FROSBridgeSrv::SrvRequest> Request = MakeShareable(new FROSBridgeSrvRospytutorialsAddTwoInts::Request(NumA, NumB));
+        TSharedPtr<FROSBridgeSrv::SrvResponse> Response = MakeShareable(new FROSBridgeSrvRospytutorialsAddTwoInts::Response());
+        UE_LOG(LogTemp, Log, TEXT("Call Service in Tick: %d + %d"), NumA, NumB);
+        Handler->CallService(ServiceClient, Request, Response);
+    }
+
+    Handler->Render();
+}
+
+void AROSBridgeTestGameModeBase::Logout(AController *Exiting)
+{
+    UE_LOG(LogTemp, Log, TEXT("On Logout() function. "));
+    // UE_LOG(LogTemp, Log, TEXT("SharedPtr RefCount = %d. "), Handler.GetSharedReferenceCount());
+    Handler->Disconnect();
+    // delete Handler;
+    delete Subscriber;
+    delete Publisher;
+    UE_LOG(LogTemp, Log, TEXT("After Logout() function. "));
+    AGameModeBase::Logout(Exiting);
+}
--- ROSBridgeTest/Source/ROSBridgeTest/ROSBridgeTestGameModeBase.h
+++ ROSBridgeTest/Source/ROSBridgeTest/ROSBridgeTestGameModeBase.h
@@ -8,9 +8,8 @@
 #include "ROSBridgeHandler.h"
 #include "ROSBridgePublisher.h"
 #include "ROSStringSubscriber.h"
+#include "ROSAddTwoIntsClient.h"
 
-// #include "ROSBridgeActor.h"
-#include "ROSStringSubscriber.h"
 #include "sensor_msgs/JointState.h"
 #include "std_msgs/Header.h"
 
@@ -28,6 +27,7 @@ public:
     FROSBridgeHandler* Handler;
     FROSStringSubScriber* Subscriber;
     FROSBridgePublisher* Publisher;
+    FROSAddTwoIntsClient* ServiceClient;
 
     AROSBridgeTestGameModeBase(const FObjectInitializer &ObjectInitializer) :
         AGameModeBase(ObjectInitializer){
@@ -35,60 +35,8 @@ public:
         PrimaryActorTick.bStartWithTickEnabled = true;
     }
 
-    void BeginPlay() override
-    {
-        Super::BeginPlay();
-        // AROSBridgeActor* Actor = GetWorld()->SpawnActor<AROSBridgeActor>(AROSBridgeActor::StaticClass());
-        Handler = new FROSBridgeHandler(TEXT("127.0.0.1"), 9001);
-        UE_LOG(LogTemp, Log, TEXT("Handler Created. "));
-
-        Subscriber = new FROSStringSubScriber(TEXT("/chatter"));
-        Handler->AddSubscriber(Subscriber);
-        UE_LOG(LogTemp, Log, TEXT("Added chatter subscriber. "));
-
-        Publisher = new FROSBridgePublisher(TEXT("sensor_msgs/JointState"), TEXT("/talker"));
-        Handler->AddPublisher(Publisher);
-
-        Handler->Connect();
-        UE_LOG(LogTemp, Log, TEXT("Connected to WebSocket server. "));
-    }
-
-    void Tick(float DeltaSeconds) override {
-        Super::Tick(DeltaSeconds);
-        UE_LOG(LogTemp, Log, TEXT("GameMode Ticks, DeltaSeconds = %.3f."), DeltaSeconds);
-
-        // FROSBridgeMsgStdmsgsString* StringMsgToSend = new
-        //     FROSBridgeMsgStdmsgsString(TEXT("New Message at ") + FDateTime::Now().ToString());
-        // Handler->PublishMsg(TEXT("/talker"), StringMsgToSend); 
-        // delete StringMsgToSend;
-
-        FROSBridgeMsgStdmsgsHeader header(GFrameCounter, FROSTime::Now(), TEXT("1"));
-        TArray<FString> names = { TEXT("head"), TEXT("torso"), TEXT("gripper") }; 
-        TArray<double > position, velocity, effort; 
-        for (int i = 0; i < 3; i++)
-        {
-            position.Add(FMath::FRandRange(0.0f, 5.0f)); 
-            velocity.Add(FMath::FRandRange(0.0f, 5.0f));
-            effort.Add(FMath::FRandRange(0.0f, 5.0f));
-        }
-        FROSBridgeMsgSensormsgsJointState *JointState
-            = new FROSBridgeMsgSensormsgsJointState(header, names, position, velocity, effort); 
-        Handler->PublishMsg(TEXT("/talker"), JointState);
-        delete JointState; 
-
-        Handler->Render();
-    }
-
-    void Logout(AController *Exiting) override
-    {
-        UE_LOG(LogTemp, Log, TEXT("On Logout() function. "));
-        // UE_LOG(LogTemp, Log, TEXT("SharedPtr RefCount = %d. "), Handler.GetSharedReferenceCount());
-        Handler->Disconnect();
-        // delete Handler;
-        delete Subscriber;
-        delete Publisher;
-        UE_LOG(LogTemp, Log, TEXT("After Logout() function. "));
-        AGameModeBase::Logout(Exiting);
-    }
+    void BeginPlay() override;
+    void Tick(float DeltaSeconds) override;
+    void Logout(AController *Exiting) override;
 
 };
