930341a7fea500800e820e197bfa38f527e1dee1
Andrei Haidu <andrei.haidu@yahoo.com>
2017-08-08 08:43:37 +0200

    master changed to plugin template

--- ROSBridgeTest/Source/ROSBridgeTest/ROSBridgeTestGameModeBase.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "ROSBridgeTest.h"
-#include "ROSBridgeTestGameModeBase.h"
-
-
-
-
--- ROSBridgeTest/Source/ROSBridgeTest/ROSStringSubscriber.cpp
+++
@@ -1,1 +0,0 @@
-#include "ROSBridgeTest.h"
