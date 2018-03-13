14df975572289a3d80b317c98daa75d1be9eee11
Andrei Haidu <andrei.haidu@yahoo.com>
2018-03-13 17:47:43 +0100

    clean plugin

---
+++ README.md
@@ -0,0 +1,6 @@
+# URoboSim
+
+Unreal engine based robot simulator.
+
+Version using `SDF` for loading robots, since `URDF` is not suitable for robot simulation.
+See `gzsdf` for converting `URDF` to `SDF`.
---
+++ Source/URoboSim/Private/URoboSim.cpp
@@ -0,0 +1,20 @@
+// Copyright 2018, Institute for Artificial Intelligence - University of Bremen
+
+#include "URoboSim.h"
+
+#define LOCTEXT_NAMESPACE "FURoboSimModule"
+
+void FURoboSimModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FURoboSimModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FURoboSimModule, URoboSim)
